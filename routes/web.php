ed0806aefd7703311a6a02c67302c411cabe0249
S-Zyk <zyksergei@gmail.com>
2022-12-05 18:57:06 +0300

    new

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,21 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [\App\Http\Controllers\FirstController::class, 'index']);
-
-Route::get('/admin', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
-Route::get('/admin/categories', [\App\Http\Controllers\Admin\CategoryController::class, 'index']);
-Route::get('/admin/categories/create', [\App\Http\Controllers\Admin\CategoryController::class, 'create'])
-    ->name('categories.creat');
-Route::post('/admin/categories/store', [\App\Http\Controllers\Admin\CategoryController::class, 'store'])
-    ->name('categories.store');
-Route::get('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'edit']);
-Route::put('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'update']);
-Route::delete('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'delete']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/', [\App\Http\Controllers\FirstController::class, 'index']);
+//
+//Route::get('/admin', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
+//Route::get('/admin/categories', [\App\Http\Controllers\Admin\CategoryController::class, 'index']);
+//Route::get('/admin/categories/create', [\App\Http\Controllers\Admin\CategoryController::class, 'create'])
+//    ->name('categories.creat');
+//Route::post('/admin/categories/store', [\App\Http\Controllers\Admin\CategoryController::class, 'store'])
+//    ->name('categories.store');
+//Route::get('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'edit']);
+//Route::put('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'update']);
+//Route::delete('/admin/categories/{id}', [\App\Http\Controllers\Admin\CategoryController::class, 'delete']);
+//
+//Auth::routes();
+//
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 /**
